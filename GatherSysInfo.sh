9fb3fb63114f7506e9e3852253487d786960acfd
Fine.Fan <fine.fan@hotmail.com>
2014-12-05 10:44:31 +0800

    Update GatherSysInfo.sh

--- GatherSysInfo.sh
+++ GatherSysInfo.sh
@@ -11,7 +11,7 @@ echo 'System brief infromation:'
 dmidecode -t system | awk 'NR>=6&&NR<=9&&NR!=8 {print $0}'
 #Get Cpu Information
 echo 'CPU brief information:'
-dmidecode -t processor | grep Version
+dmidecode -t processor | grep Version:
 #Get Memory Information
 echo 'Memory brief information:'
 dmidecode -t 17 | grep Size
@@ -36,10 +36,22 @@ awk '{print "       ",$0}' /etc/redhat-release
 #The kernel version
 echo 'Kernel Version:'
 uname -a | awk '{print "       ",$3}' #There should be seven speaces
+#IP information
+echo 'IP Information:'
+ip addr | awk '{print "       ",$0}' #There should be seven speaces
+#Route Information
+echo 'Route Information:'
+route -n | awk 'NR>1 {print "       ",$0}' #There should be seven speaces
 #Disk useage 
 echo 'The disk useage:'
 df -h --total | awk '{print "       ",$0}' #There should be seven speaces
 #Login user
 echo 'Login user brief information:'
 echo -e '\t'`awk -F : '$3 >= 500 {print "Line:",NR,$0}' /etc/passwd | grep -v nologin`
+#Bridge Information
+echo 'Show Bridge Information:'
+brctl show | awk '{print "       ",$0}' #There should be seven speaces
+#List the KVM machines
+echo 'The KVM on this machines:'
+virsh list --all | awk '{print "       ",$0}' #There should be seven speaces
 #OS Information End@@@@@@@@@@@@@@@@@@@
